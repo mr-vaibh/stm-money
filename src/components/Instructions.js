e087dbadfd58d4abc090d6923a5b8b45733ecd64
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2023-12-24 16:42:17 +0530

    FIXED: faling App.test.js

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -21,7 +21,7 @@ export default function Instructions({ noOfPeople }) {
                     aria-controls="panel1a-content"
                     id="panel1a-header"
                 >
-                    <Typography style={{ display: 'flex', alignItems: 'center', fontStyle: 'italic' }}><LightbulbIcon style={{ color: '#eb9800' }} /> Instruction</Typography>
+                    <Typography style={{ display: 'flex', alignItems: 'center', fontStyle: 'italic' }}><LightbulbIcon style={{ color: '#eb9800' }} /> Instructions</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
                     <Typography component={'ul'}>
